f3e8de0f29634b30a092145c91e7f16c675e7175
Shiva Sai Kiran <shivasaikirann1@gmail.com>
2026-03-31 00:51:06 +0530

    UC1: Application Entry & Welcome Message

---
+++ App/src/UseCase1HotelBookingApp.java
@@ -0,0 +1,13 @@
+/**
+ * Hotel Booking Application
+ * Use Case 1: Application Entry & Welcome Message
+ * @author Shiva
+ * @version 1.0
+ */
+public class UseCase1HotelBookingApp {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Hotel Booking Management System");
+        System.out.println("Version: 1.0");
+    }
+}
